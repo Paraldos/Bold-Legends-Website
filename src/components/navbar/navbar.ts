7a59f452c192224ab70e024ffa0cfd05c0c589dd
Schiggy <hillbrecht.s@gmail.com>
2025-10-30 21:39:02 +0100

    added event listener to navbar

--- src/components/navbar/navbar.ts
+++ src/components/navbar/navbar.ts
@@ -3,39 +3,27 @@ import "./navbar.css";
 export default class Navbar {
   constructor(root: HTMLElement) {
     this.initHtml(root);
-    const home = root.querySelector(".navbar__home");
-    const downloads = root.querySelector(".navbar__downloads");
-    const tools = root.querySelector(".navbar__tools");
-    this.addListeners(home, downloads, tools);
+    this.addListeners();
   }
 
   initHtml(root: HTMLElement) {
     root.innerHTML = `
 		<nav>
 			<ul class="navbar">
-				<li class="navbar__home">Home</li>
-				<li class="navbar__downloads">Downloads</li>
-				<li class="navbar__tools selected">Tools</li>
+				<li class="navbar__home navbar__link">Home</li>
+				<li class="navbar__downloads navbar__link">Downloads</li>
+				<li class="navbar__tools navbar__link selected">Tools</li>
 			</ul>
 		</nav>
 	`;
   }
 
-  addListeners(
-    home: Element | null,
-    downloads: Element | null,
-    tools: Element | null
-  ) {
-    home?.addEventListener("click", () => {
-      console.log("home");
-    });
-
-    downloads?.addEventListener("click", () => {
-      console.log("downloads");
-    });
-
-    tools?.addEventListener("click", () => {
-      console.log("tools");
+  addListeners() {
+    const navLinks = document.querySelectorAll(".navbar__link");
+    navLinks.forEach((el) => {
+      el.addEventListener("click", () => {
+        console.log(el);
+      });
     });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ class Main {
   constructor() {
     const app = document.querySelector<HTMLDivElement>("#app")!;
     new Navbar(app);
+    new SectorTool(app);
   }
 }
 
