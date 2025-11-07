b5e5f05138ffbf069fff09f214dac38f41682e14
Schiggy <hillbrecht.s@gmail.com>
2025-11-07 15:53:27 +0100

    basic setup map legend

--- src/components/sectorTool/mapLegend.ts
+++ src/components/sectorTool/mapLegend.ts
@@ -1,5 +1,19 @@
 import Sector from "./sector.ts";
 
 export default class MapLegend {
-  constructor(sector: Sector) {}
+  sectorTool = document.querySelector(".sectorTool") as HTMLElement | null;
+  legend: HTMLElement | null;
+  sector: Sector;
+
+  constructor(sector: Sector) {
+    this.sector = sector;
+    this.legend = this.initMapLegend();
+  }
+
+  initMapLegend(): HTMLElement | null {
+    if (!this.sectorTool) return null;
+    const legend = document.createElement("div");
+    legend.classList.add("sectorTools__legend");
+    return legend;
+  }
 }
