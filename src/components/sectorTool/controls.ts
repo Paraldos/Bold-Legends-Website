bd8a1c9a464641ef1187847a4f4d47c277e4ece5
Schiggy <hillbrecht.s@gmail.com>
2025-11-07 22:38:08 +0100

    basic setup controls

---
+++ src/components/sectorTool/controls.ts
@@ -0,0 +1,39 @@
+import Sector from "./sector/sector.ts";
+import "./controls.css";
+
+export default class Controls {
+  sectorTool = document.querySelector(".sectorTool") as HTMLElement | null;
+  sector: Sector;
+
+  constructor(sector: Sector) {
+    this.sector = sector;
+    this.addControls();
+  }
+
+  addControls() {
+    const div = document.createElement("div");
+    div.classList = "sectorTools__controls";
+    this.addNewBtn(div);
+    this.addSaveBtn(div);
+    this.addLoadBtn(div);
+    this.sectorTool?.appendChild(div);
+  }
+
+  addNewBtn(parent: HTMLElement) {
+    const saveBtn = document.createElement("button");
+    saveBtn.innerHTML = "New";
+    parent.appendChild(saveBtn);
+  }
+
+  addSaveBtn(parent: HTMLElement) {
+    const saveBtn = document.createElement("button");
+    saveBtn.innerHTML = "Save";
+    parent.appendChild(saveBtn);
+  }
+
+  addLoadBtn(parent: HTMLElement) {
+    const saveBtn = document.createElement("button");
+    saveBtn.innerHTML = "Load";
+    parent.appendChild(saveBtn);
+  }
+}
--- src/components/sectorTool/sectorTool.ts
+++ src/components/sectorTool/sectorTool.ts
@@ -3,6 +3,7 @@ import ContentComponent from "../contentComponent/contentComponent.ts";
 import Sector from "./sector/sector.ts";
 import Map from "./map.ts";
 import Legend from "./legend.ts";
+import Controls from "./controls.ts";
 
 export default class SectorTool extends ContentComponent {
   sector = new Sector();
@@ -15,7 +16,10 @@ export default class SectorTool extends ContentComponent {
 
   fill() {
     this.contentContainer.innerHTML = `<div class="sectorTool"></div>`;
-    if (this.sector) new Map(this.sector);
-    if (this.sector) new Legend(this.sector);
+    if (this.sector) {
+      new Controls(this.sector);
+      new Map(this.sector);
+      new Legend(this.sector);
+    }
   }
 }
