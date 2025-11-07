9014b004c061f15367ba8531601effe145e5a0f9
Schiggy <hillbrecht.s@gmail.com>
2025-11-07 15:57:33 +0100

    adding hexes to legend

--- src/components/sectorTool/mapLegend.ts
+++ src/components/sectorTool/mapLegend.ts
@@ -8,6 +8,7 @@ export default class MapLegend {
   constructor(sector: Sector) {
     this.sector = sector;
     this.legend = this.initMapLegend();
+    this.addHexes();
   }
 
   initMapLegend(): HTMLElement | null {
@@ -15,5 +16,12 @@ export default class MapLegend {
     const legend = document.createElement("div");
     legend.classList.add("sectorTools__legend");
     return legend;
+  }
+
+  addHexes(): void {
+    if (!this.legend) return;
+    this.sector.hexes.forEach((hex) => {
+      console.log(hex);
+    });
   }
 }
