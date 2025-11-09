79dbc18381025ccfa72eee6fe9bc415682733c33
Schiggy <hillbrecht.s@gmail.com>
2025-11-09 09:31:20 +0100

    added save and load btns

--- src/components/sectorTool/saveLoadModal/saveLoadmodal.ts
+++ src/components/sectorTool/saveLoadModal/saveLoadmodal.ts
@@ -4,5 +4,27 @@ import Sector from "../sector/sector";
 export default class SaveLoadModal extends Modal {
   constructor() {
     super();
+    for (let i = 0; i < 5; i++) {
+      this.addSlot(i);
+    }
+  }
+
+  addSlot(i: number) {
+    const div = document.createElement("div");
+    div.classList = "slot";
+
+    const p = document.createElement("p");
+    p.innerHTML = `Slot ${i + 1}`;
+    div.appendChild(p);
+
+    const loadBtn = document.createElement("button");
+    loadBtn.innerHTML = "Load";
+    div.appendChild(loadBtn);
+
+    const saveBtn = document.createElement("button");
+    saveBtn.innerHTML = "Save";
+    div.appendChild(saveBtn);
+
+    this.contentContainer.appendChild(div);
   }
 }
